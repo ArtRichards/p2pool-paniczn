1a92228c80e80af21c22c18dd380307489971bec
paniczklos <panicz.klos@gmail.com>
2014-03-24 21:23:56 +0100

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -226,7 +226,29 @@ nets = dict(
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
-    ),    
+    ), 
+        planetdollar=math.Object(
+        P2P_PREFIX='fbc0b6db'.decode('hex'),
+        P2P_PORT=7784,
+        ADDRESS_VERSION=55,
+        RPC_PORT=7783,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'planetdollaraddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 100*100000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('vtc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=150, # s
+        SYMBOL='PDR',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'],
+            'planetdollar') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/planetdollar/') if platform.system() == 'Darwin' else os.path.expanduser('~/.planetdollar'), 'planetdollar.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://cryptexplorer.com/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://cryptexplorer.com/address/',
+        TX_EXPLORER_URL_PREFIX='http://cryptexplorer.com/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),  
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
